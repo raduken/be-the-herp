8a7db109d01cd5588f8b0fc51346ba99fc58fc89
raduken <raphaelmendes1984@gmail.com>
2020-03-27 02:26:56 -0300

    back-end da aplicação

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,13 @@
+const Connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response){
+        const ong_id = request.headers.authorization;
+
+        const incidents = await Connection('incidents')
+        .where('ong_id', ong_id)
+        .select('*');
+
+        return response.json(incidents);
+    }
+}
