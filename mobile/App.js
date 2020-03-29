10b94e3b2d40ff1003ed13fc650df232ccc36797
RAPHAEL DE ABREU MENDES <raphaelmendes1984@gmail.com>
2020-03-29 19:55:11 -0300

    mobile

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './scr/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
